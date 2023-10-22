af70db37525111ef7949025931b1dc09ed1615fb
lxne <lxne@users.noreply.github.com>
2023-10-22 15:35:15 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -332,7 +332,7 @@ async function playLoved (){
 			}
 			//await self.page.evaluate(()=>document.querySelector('#page_sidebar a.sidebar-nav-link[href$="loved"]').click()); // zu Lieblingssongs wechseln
 			self.page.click('#page_sidebar a.sidebar-nav-link[href$="loved"]'); // zu Lieblingssongs wechseln
-		//	await delay(5000);
+			await delay(500);
 			await self.page.waitForSelector('#page_content div.loved-heading div[data-testid="play"]');
 			await self.page.evaluate(()=>document.querySelector('#page_content button.chakra-button[data-testid="playlist-play-button"]').click()); // Abspielen
 	//		await self.page.waitForSelector('#page_player button[aria-label*="epeat"]');
