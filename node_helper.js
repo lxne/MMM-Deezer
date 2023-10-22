3d31431a7b21ce7b7d6037259f36b4e06b8a9148
lxne <lxne@users.noreply.github.com>
2023-10-22 14:18:27 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -332,8 +332,9 @@ async function playLoved (){
 			}
 			//await self.page.evaluate(()=>document.querySelector('#page_sidebar a.sidebar-nav-link[href$="loved"]').click()); // zu Lieblingssongs wechseln
 			await self.page.click('#page_sidebar a.sidebar-nav-link[href$="loved"]'); // zu Lieblingssongs wechseln
-			await self.page.waitForSelector('#page_content button.chakra-button[data-testid="playlist-play-button"]');
-			await delay(5000);
+			await delay(1000);
+			await self.page.waitForSelector('#page_content div.loved-heading div[data-testid="play"]');
+			await delay(1000);
 			await self.page.evaluate(()=>document.querySelector('#page_content button.chakra-button[data-testid="playlist-play-button"]').click()); // Abspielen
 			await self.page.waitForSelector('#page_player button[aria-label*="epeat"]');
 			await self.page.evaluate(()=>document.querySelector('#page_player button[aria-label="Turn off repeat"]').click()); // EN-Selector; Klicken falls Ein-Song-Wiederholung an
